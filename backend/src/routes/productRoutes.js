bf6018215208995f8b6c8048a0c06a2a3ca2fadb
HiM1803 <himanshu180305@gmail.com>
2025-10-12 22:17:41 +0530

    minor changes in product rooute

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import addProduct from '../controllers/productController.js';
+import upload from '../middlewares/multer.js'
 
 const router = Router();
 
-router.route("/addProduct").post(addProduct)
+router.route("/addProduct").post(upload.single('image'),addProduct)
 
 export default router
