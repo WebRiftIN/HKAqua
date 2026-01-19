58d93efcb544307eb73e7b16ae7fc247a216cfd0
Bharat56 <grasontech@gmail.com>
2026-01-19 18:05:54 +0530

    changes in viteconfig.js

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,27 +1,13 @@
-// Added proxy for /api to backend on port 3000
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
+    port: 5173,
+    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
@@ -30,4 +16,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
